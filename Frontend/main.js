03b16fca39ccaace41ab98f198c6dfd98416c54f
dudarib <dudarib@outlook.com>
2021-08-06 03:05:01 +0100

    added User Profile and editing option

--- Frontend/main.js
+++ Frontend/main.js
@@ -2,10 +2,12 @@ Moralis.initialize("zhtFOvVlO90ncDMdyBW4MXhPwULckUeLQ11BmmKA");
 Moralis.serverURL = 'https://2cvjfiew4kxo.usemoralis.com:2053/server'
 
 init = async () => {
+    hideElement(userInfo);
     window.web3 = await Moralis.Web3.enable();
     initUser();
 }
 
+/**Takes care of displaying the correct button */
 initUser = async () => {
     if(await Moralis.User.current()){
         hideElement(userConnectButton);
@@ -25,6 +27,21 @@ login = async () => {
     }
 }
 
+logout = async () => {
+    await Moralis.User.logOut();
+    hideElement(userInfo);
+    initUser();
+}
+
+openUserInfo = async () => {
+    user = await Moralis.User.current();
+    if (user){
+        showElement(userInfo);
+    }else{
+        login();
+    }
+}
+
 hideElement = (element) => element.style.display = "none";
 showElement = (element) => element.style.display = "block";
 
@@ -32,6 +49,11 @@ const userConnectButton = document.getElementById("btnConnect");
 userConnectButton.onclick = login;
 
 const userProfileButton = document.getElementById("btnUserInfo");
+userProfileButton.onclick = openUserInfo;
 
+const userInfo = document.getElementById("userInfo");
+
+document.getElementById("btnCloseUserInfo").onclick = () => hideElement(userInfo);
+document.getElementById("btnLogout").onclick = logout;
 
 init();
