7eebff5c587ce6fc8f21e3c62d437dcbd98e829b
dudarib <dudarib@outlook.com>
2021-09-01 16:23:55 +0100

    saving user Profiles

--- Frontend/main.js
+++ Frontend/main.js
@@ -36,10 +36,41 @@ logout = async () => {
 openUserInfo = async () => {
     user = await Moralis.User.current();
     if (user){
+        const email = user.get('email');
+        if(email){
+            userEmailField.value = email;
+        }else{
+            userEmailField.value = "";
+        }
+
+        userUsernameField.value = user.get('username');
+
+        const userAvatar = user.get('avatar');
+        if(userAvatar){
+            userAvatarImg.src = userAvatar.url();
+            showElement(userAvatarImg);
+        }else{
+            hideElement(userAvatarImg);
+        }
+
         showElement(userInfo);
     }else{
         login();
     }
+}
+
+saveUserInfo = async () => {
+    user.set('email', userEmailField.value);
+    user.set('username', userUsernameField.value);
+
+    if (userAvatarFile.files.length > 0) {
+        const avatar = new Moralis.File("avatar.jpg", userAvatarFile.files[0]);
+        user.set('avatar', avatar);
+      }
+
+      await user.save();
+      alert("user info saved sucessfull");
+      openUserInfo();
 }
 
 hideElement = (element) => element.style.display = "none";
@@ -52,8 +83,15 @@ const userProfileButton = document.getElementById("btnUserInfo");
 userProfileButton.onclick = openUserInfo;
 
 const userInfo = document.getElementById("userInfo");
+const userUsernameField = document.getElementById("txtUsername");
+const userEmailField = document.getElementById("txtEmail");
+const userAvatarImg = document.getElementById("imgAvatar");
+const userAvatarFile = document.getElementById("fileAvatar");
+
 
 document.getElementById("btnCloseUserInfo").onclick = () => hideElement(userInfo);
 document.getElementById("btnLogout").onclick = logout;
+document.getElementById("btnSaveUserInfo").onclick = saveUserInfo;
+
 
 init();
